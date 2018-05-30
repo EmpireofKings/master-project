88c1a28b6b401cad9e9c655c7f31ff2594f1f685
bhogstad <36335034+bhogstad@users.noreply.github.com>
2018-05-30 17:09:06 +0200

    Implemented 1-step Q-learning and updated time

--- code/main/actor_critic/Actor.py
+++ code/main/actor_critic/Actor.py
@@ -1,12 +1,10 @@
 """
-Policy Gradient Reinforcement Learning
+Deep Q-learning
 Uses a 3 layer neural network as the policy network
 
 """
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
 
 
 class PolicyGradient:
@@ -14,47 +12,51 @@ class PolicyGradient:
                  input_size,
                  output_size,
                  learning_rate,
-                 reward_decay):
+                 value_decay,
+                 regul_factor):
         self.n_x = input_size
         self.n_y = output_size
         self.lr = learning_rate
-        self.discount_factor = reward_decay
-
+        self.discount_factor = value_decay
+        self.regul_factor = regul_factor
+        
         self.episode_states = np.empty((0,input_size), np.float32)
-        self.episode_actions = []
         self.episode_rewards = []
+        self.episode_next_values = []
+        self.episode_actions = []
 
         self.build_network()
 
-        self.cost_history = []
-
         self.sess = tf.Session()
-
-        # $ tensorboard --logdir=_logs_
-        # http://0.0.0.0:6006/
-        tf.summary.FileWriter("_logs_/", self.sess.graph)
 
         self.sess.run(tf.global_variables_initializer())
 
         # 'Saver' op to save and restore all the variables
-        self.saver = tf.train.Saver()
+#        self.saver = tf.train.Saver()
 
-    def store_transition(self, s, a, r):
+
+    def store_transition(self, s, r, a, v):
         """
             Store play memory for training
 
             Arguments:
                 s: state
-                a: index of action taken
                 r: reward after action
+                a: action taken
+                v: values for next state using previous weights
         """
+        
         self.episode_states = np.vstack((self.episode_states, s))
+        
         self.episode_rewards.append(r)
-
-        # Store actions as list of arrays, hot-one encoding at each transition
+        
+        # Store actions as list of arrays, one-hot encoding at each transition
         action = np.zeros(self.n_y, dtype=np.float32)
         action[a] = 1
         self.episode_actions.append(action)
+        
+        
+        self.episode_next_values.append(v)
 
 
     def choose_action(self, observation):
@@ -67,110 +69,188 @@ class PolicyGradient:
             Returns: index of action we want to choose
         """
         
-        # Reshape necessary for as tesseract expects the rows as columns
         observation = observation.reshape(1, -1)
         
-        # Run forward propagation to get softmax probabilities
-        prob_weights = self.sess.run(self.network, feed_dict={self.X: observation})
+        # Run forward propagation to get values of the action outputs
+        values = self.sess.run(self.network, feed_dict={self.X: observation})
+
+        # return the index of the action which results in the best expected reward
+        return np.argmax(values)
+
+
+    def action_values(self, observation):
         
-        # Select action using a biased sample
-        # this will return the index of the action we've sampled
-        return np.argmax(prob_weights)
-        #np.random.seed(1)
-        #return np.random.choice(range(self.n_y), p=prob_weights.ravel())
+        observation = observation.reshape(1, -1)
+        
+        # Run forward propagation to get values of the action outputs
+        values = self.sess.run(self.network, feed_dict={self.X: observation})
+
+        # return the index of the action which results in the best expected reward
+        return values
+        
+
+    def max_value_action(self, observation):
+        """
+            Choose action based on observation
+
+            Arguments:
+                observation: array of state, has shape (,num_features)
+
+            Returns: max value of possible actions
+        """
+        
+        observation = observation.reshape(1, -1)
+        
+        # Run forward propagation to get Q-values of each action output
+        values = self.sess.run(self.network, feed_dict={self.X: observation})
+        
+        # this will return the max value of the possible actions
+        return np.max(values)
+
 
     def learn(self):
-        # Discount episode reward
-        discounted_rewards = self.discount_rewards()
-
-        ## DEBUG OUTPUT commented out
-        #print("episode rewards", self.episode_rewards)
-        #print("losses", self.sess.run(self.loss, feed_dict={
-        #                        self.X: self.episode_states,
-        #                        self.actions_taken: self.episode_actions,
-        #                        self.discounted_rewards: discounted_rewards}))
-        #print("discount rewards\n", discounted_rewards)
-        #print("action values\n", self.episode_actions)
-        #print("episode_states\n", self.episode_states)
-        
-        #print("policy loss", self.sess.run(self.policy_loss, feed_dict={
-        #                            self.actions_taken: self.episode_actions,
-        #                            self.discounted_rewards: discounted_rewards}))
-        
-        #test_state = np.zeros((1, self.n_x))
-        #test_state[0,0] = 1
-        
-        #print("test_state", test_state)
-        #print("Before learning:", self.sess.run(self.network, feed_dict={self.X: test_state}))
-        ## DEBUG END
-        
         # Train on available data    
         loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={
                             self.X: self.episode_states,
+                            self.values: self.discount_values(),
                             self.actions_taken: self.episode_actions,
-                            self.discounted_rewards: discounted_rewards})
+                            self.rewards: np.array(self.episode_rewards).reshape(-1,1)})
 
-        #print("After learning:", self.sess.run(self.network, feed_dict={self.X: test_state}))
-        
+
     def reset(self):
         # Reset the episode data, clear memory
         self.episode_states = np.empty((0,self.n_x), np.float32)
-        self.episode_actions = []
         self.episode_rewards  = []
-
-    def discount_rewards(self):
-        count_steps = len(self.episode_rewards)
-        # Not all rewads should be negative if the last is negative
-        # TODO: Maybe not needed because of normalization
-        last_reward = self.episode_rewards[-1] if self.episode_rewards[-1] > 0 else 0
-        discounted_rewards = np.zeros((count_steps+1))
-        discounted_rewards[-2] = self.episode_rewards[-1]
+        self.episode_actions = []
+        self.episode_next_values = []
         
-        for t in range(count_steps - 1):
-            discounted_rewards[t] = last_reward * self.discount_factor**(count_steps-t) + self.episode_rewards[t]
         
-        discounted_rewards[-1]=10
+    def discount_values(self):
+        """Discount the value of the best action in the following state
+        using previous parameters (calculated in the run_simulator).
+        This is used in the loss function"""
         
-        discounted_rewards = discounted_rewards.reshape(-1, 1)
-        return MinMaxScaler().fit(discounted_rewards).transform(discounted_rewards)[:count_steps]
-
+        discounted_values = []
+                
+        for i in range(len(self.episode_next_values)):
+            discounted_values.append((self.episode_next_values[i]*self.discount_factor))
         
+        discounted_values = np.array(discounted_values).reshape(-1,1)
+        
+        return discounted_values
+        
+    
     def build_network(self):
         # Create placeholders
         with tf.name_scope('inputs'):
             self.X = tf.placeholder(tf.float32, shape=[None, self.n_x], name="X")
+
+            self.values = tf.placeholder(tf.float32,
+                                                shape=[None, 1],
+                                                name="next_values") 
+            
+            self.rewards = tf.placeholder(tf.float32,
+                                                shape=[None, 1],
+                                                name="rewards") 
+            
             self.actions_taken = tf.placeholder(tf.float32,
                                                 shape=[None, self.n_y],
                                                 name="actions_taken")
-            self.discounted_rewards = tf.placeholder(tf.float32,
-                                                     shape=[None, 1],
-                                                     name="discounted_rewards")
-            
-        # First hidden layer
-        layer_1 = tf.layers.Dense(units=30,  # num output nodes
+        """Variables"""
+        inputs = self.n_x
+        nodes_l1 = int(self.n_x)
+        nodes_l2 = int(self.n_x/2)
+        outputs = self.n_y
+        
+        
+        
+        
+        """First hidden layer"""
+        layer_1 = tf.layers.Dense(units=nodes_l1,
                                   activation=tf.nn.relu,
+                                  kernel_initializer=tf.variance_scaling_initializer(
+                                          scale=2.0,
+                                          mode="fan_avg",
+                                          distribution="uniform",
+                                          dtype=tf.float32),
                                   name="layer_1")
 
-        # Raw output layer, therefore no activation function
-        # Also called logits sometimes, don't know why
-        layer_2 = tf.layers.Dense(units=self.n_y,  # num output nodes
+        
+        """Second hidden layer"""
+        layer_2 = tf.layers.Dense(units=nodes_l2,
+                                  activation = tf.nn.relu,
+                                  kernel_initializer=tf.variance_scaling_initializer(
+                                          scale=2.0,
+                                          mode="fan_avg",
+                                          distribution="uniform",
+                                          dtype=tf.float32),
                                   name="layer_2")
+        
+        """Output layer"""
+        layer_3 = tf.layers.Dense(units = outputs,
+                                  kernel_initializer=tf.variance_scaling_initializer(
+                                          scale=1.0,
+                                          mode="fan_avg",
+                                          distribution="uniform",
+                                          dtype=tf.float32),
+                                  name = "layer_3")
 
-        self.network = tf.nn.softmax(layer_2(layer_1(self.X)), name="layer_softmax_out")
+        self.network = layer_3(layer_2(layer_1(self.X)))
         
-        ## Loss
-        # Element-wise multiplication, to touch only values for which we actually took an action
-        # Note that the discounded rewards are normalized between 0 and 1 in order to fit to the softmax
-        #    output of the network
-        # -> actions: [0, 1, 0, 0] * 0.1 => [0, 0.1, 0, 0]
-        # If the softmmax outup is [0, 0.9, 0, 0] but we have a low (negative before normalization) reward
-        #   this value (0.9) will be decreased
-        self.policy_loss = tf.multiply(self.actions_taken, self.discounted_rewards)
+#        
+#        
+#        
+#        """First hidden layer"""
+#        weights_1 = tf.Variable(tf.random_uniform((inputs,nodes_l1),
+#                                                  minval=-2/np.sqrt(inputs),
+#                                                  maxval=2/np.sqrt(inputs)))
+#        biases_1 = tf.Variable(tf.random_uniform((1,nodes_l1),
+#                                                 minval=-1/np.sqrt(nodes_l1),
+#                                                 maxval=1/np.sqrt(nodes_l1)))
+#        h1_output = tf.nn.relu(tf.matmul(self.X,weights_1)+biases_1)
+#        
+#        
+#        """Second hidden layer"""
+#        weights_2 = tf.Variable(tf.random_uniform((nodes_l1,nodes_l2),
+#                                                  minval=-2/np.sqrt(nodes_l1),
+#                                                  maxval=2/np.sqrt(nodes_l1)))
+#        biases_2 = tf.Variable(tf.random_uniform((1,nodes_l2),
+#                                                 minval=-1/np.sqrt(nodes_l2),
+#                                                 maxval=1/np.sqrt(nodes_l2)))
+#        h2_output = tf.nn.relu(tf.matmul(h1_output,weights_2)+biases_2)
+#        
+#        
+#        """Output layer"""
+#        weights_output = tf.Variable(tf.random_uniform((nodes_l2,outputs),
+#                                                  minval=-2/np.sqrt(nodes_l2),
+#                                                  maxval=2/np.sqrt(nodes_l2)))
+#        biases_output = tf.Variable(tf.random_uniform((1,outputs),
+#                                                 minval=-1/np.sqrt(outputs),
+#                                                 maxval=1/np.sqrt(outputs)))
+#        nn_output = tf.matmul(h2_output,weights_output)+biases_output
+#        
+#        self.network = nn_output
+
         
-        # sum((y_hat-y_true)**2)
-        # Touch only values for which we actually took an action
-        y_hat_single_action = tf.multiply(self.actions_taken, self.network)
-        self.loss = tf.reduce_sum((y_hat_single_action - self.policy_loss)**2)
+        """Regularization L2 and Entropy"""
+#        layer1_reg = tf.nn.l2_loss(weights_1)
+#        layer2_reg = tf.nn.l2_loss(weights_2)
+#        output_reg = tf.nn.l2_loss(weights_output)
+        
+        entropy = tf.reduce_sum(tf.log(tf.nn.softmax(self.network))*tf.nn.softmax(self.network))
+        entropy *= self.regul_factor
+#        regul =  layer1_reg + layer2_reg + output_reg + entropy
+
+
+        """Loss"""
+        #Reward + discounted value of best next action using 
+        # previous weights - current best action and weights
+        y_hat_1_act = tf.multiply(self.actions_taken, self.network)
+        val_1_act = tf.multiply(self.actions_taken, self.values)
+        rew_1_act = tf.multiply(self.actions_taken, self.rewards)
+        entropy_1_act = tf.multiply(self.actions_taken, entropy)
+        
+        self.loss = tf.square(rew_1_act + val_1_act - y_hat_1_act) + entropy_1_act
         
         with tf.name_scope('train'):
             self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
--- code/main/actor_critic/run_simulator.py
+++ code/main/actor_critic/run_simulator.py
@@ -1,19 +1,20 @@
 from Actor import PolicyGradient
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 import sys, os
-sys.path.append(os.path.abspath("../simulator"))#Change path to be wherever Simulator.py is stored locally
+sys.path.append(os.path.abspath("../simulator")) #This finds where Simulator.py is located
 
 from Simulator import *
 
+
+"""Hyperparameters"""
 np.random.seed(1)
 
-EPISODES = 150
-rewards = []
+EPISODES = 1000
 
-gridsize = [10, 10]
+gridsize = [5, 10]
 grid_flat = gridsize[0] * gridsize[1]
+
 # 1 for drone location, 1 for map of visited locations
 num_channels = 2
 input_size = grid_flat * num_channels
@@ -21,135 +22,191 @@ input_size = grid_flat * num_channels
 action_size = 4 #number of actions to choose from
 e_greedy = 0.4
 grid_seed = 1
-max_iterations = int(grid_flat/2) #it will take this many actions to go to half of the cells in the grid
+max_iterations = int(grid_flat/2)
+val_decay = 0.25
+l_rate = 0.001
+reg_factor = 0.1
 
+num_exec= 3
 
-def move_and_get_reward(drone, action_idx, disc_map):
+def move_and_get_reward(drone, action_idx, disc_map,itr):
     """Move the drone and get the reward."""
+    cost = (itr+1)/(max_iterations*50)
+    
+    location_point = drone.get_position()
+    location = location_point.get_y() * gridsize[1] + location_point.get_x()
+    bad_move = disc_map[location]/(max_iterations*100)
+    
     try:
         drone.move(Direction(action_idx))
         if "T" in drone.observe_surrounding():
-            # arbitrary reward for finding target
-            return 10
+            # arbitrary reward for finding target: This will always be positive
+            return max_iterations/100 - cost
         else:
             # if a drone has been to a location multiple times,
             # the penalty will increase linearly with each visit
-            location_point = drone.get_position()
-            location = location_point.get_y() * gridsize[1] + location_point.get_x()
-            return -disc_map[location]-1
-            #return 0.1
-    except (PositioningError, IndexError):
-        # We hit an obstacle or tried to exit the grid
-        return -10 # arbitrary
-            
+            return -bad_move - cost
 
+    except (PositioningError, IndexError):
+        # We hit an obstacle or tried to exit the grid (-max_itr / 10)
+        # Penalty is arbitrary but includes the cost and the bad_move penalty
+        return -1/10 - bad_move - cost # arbitrary
+            
 
 if __name__ == "__main__":
-    reward_list = []
-    target_found = 0    
-    PG = PolicyGradient(input_size, action_size,
-                        learning_rate=0.01,
-                        reward_decay=0.99)
     
-    global_disc_map = np.zeros(grid_flat)
+    reward_list = [] #Reward sum of each Test Episode
     
-    
-    
-    for episode in range(EPISODES):
-#        print("Episode", episode, end="\r")
-                
-        # Setup simulator
-        grid = Grid(gridsize[0],gridsize[1], seed=grid_seed)
-        grid.set_obstacles(0) #No obstacles to start
-        drone = Drone(grid, position=Point(0,0), id=99)
-        grid.set_target()
-        
-        disc_map = np.zeros(grid_flat)
+    for i in range(0,num_exec):
 
-        # Generate traning data
-        for itr in range(max_iterations):
-            # 1. Get the current state
-            drone_loc = grid.get_drones_vector()
+        
+        action_values = []
+    
+        targets_found_test = 0 #counter for number of times the target has been found in Test Episodes
+        targets_found = 0 #counter for number of times the target has been found in Train Episodes
+        
+        PG = PolicyGradient(input_size, action_size,
+                            learning_rate=l_rate,
+                            value_decay=val_decay,
+                            regul_factor = reg_factor)
+        
+        global_disc_map = np.zeros(grid_flat)
+        
+        PG_old = PG
+        
+        for episode in range(EPISODES):
             
-            # Use smaller state space for the beginning
-            disc_map += drone_loc
+            """Training Environment"""
+            # Setup simulator
+            grid = Grid(gridsize[0],gridsize[1], seed=grid_seed)
+            grid.set_obstacles(0) #No obstacles to start
+            drone = Drone(grid, position=Point(0,0), id=99)
+            grid.set_target()
+            
+            drone_loc = grid.get_drones_vector()
+            disc_map = np.zeros(grid_flat)
             state = np.append(drone_loc, disc_map)
-            
-            # 2. Choose an action based on observation
-            if np.random.randint(1,100)/100 < e_greedy:
-                action_idx = np.random.randint(0, action_size)
-            else:
-                action_idx = PG.choose_action(state)
+            initial_state = state
+            target_found=False
+            # Generate traning data
+            for itr in range(max_iterations):
                 
-            # 3. Take action in the environment
-            reward = move_and_get_reward(drone, action_idx, disc_map)
-            
-            # 4. Store transition for training
-            PG.store_transition(state, action_idx, reward)
-            
-            # 5. Check to see if target has been found
-            if reward >= 1:
-                disc_map += grid.get_drones_vector()
-                target_found+=1
-                break
-        
-        
-
-        global_disc_map += disc_map
-        #print("\n-------TRAINING------\n")
-        
-        # 5. Train neural network
-        PG.learn()
-        
-#        reward_list.append(np.sum(PG.episode_rewards))
-        
-        #Reset the action, state, and reward lists. 
-        #Keeping this is like replay memory (full sampling)
-        PG.reset()
-        
-        grid = Grid(gridsize[0],gridsize[1], seed=grid_seed)
-        grid.set_obstacles(0) #No obstacles to start
-        drone = Drone(grid, position=Point(0,0), id=99)
-        grid.set_target()
-        rewards = 0
-        disc_map = np.zeros(grid_flat)
+                # Choose an action based on observation
+                if np.random.randint(1,100)/100 < e_greedy:
+                    action_idx = np.random.randint(0, action_size)
+                else:
+                    action_idx = PG.choose_action(state)
+                
+                
+                # Take action in the environment
+                reward = move_and_get_reward(drone, action_idx, disc_map,itr)
+                
+                if reward >= 0:
+                    target_found = True
+                
+                
+                # Update the state
+                drone_loc = grid.get_drones_vector()
+                disc_map += drone_loc
+                state_next = np.append(drone_loc, disc_map)
     
-        # Run simulator
-        for itr in range(max_iterations):
-            # 1. Get the current state
-            drone_loc = grid.get_drones_vector()
+                
+                # Calculate Next Action Value
+                n_value = PG_old.max_value_action(state_next)
+                if target_found:
+                    n_value = 0
+    
+    
+                # Store transition for training
+                PG.store_transition(state, reward, action_idx, n_value)
+                state = state_next
+                
+                
+                """Enable the following to have iterative updates"""
+                PG_Old = PG
+                PG.learn()
+                PG.reset() #Disabling this is episodic replay memory (full sampling)
+                
+                
+                # End Episode if target has been found
+                if target_found:
+                    targets_found+=1
+                    break
             
-            # Use smaller state space for the beginning
+            action_values.append(np.array(PG.action_values(initial_state)).reshape(-1,4))
+            global_disc_map += disc_map
+    
+    
+            """Enable the following to have episodic updates"""
+    #        PG_old = PG
+    #        PG.learn()
+    #        PG.reset() #Disabling this is replay memory (full sampling)
+            
+            
+            
+            """Testing Environment"""
+            #Reset Grid and variables
+            grid = Grid(gridsize[0],gridsize[1], seed=grid_seed)
+            grid.set_obstacles(0) #No obstacles to start
+            drone = Drone(grid, position=Point(0,0), id=99)
+            grid.set_target()
+            rewards = 0
+            
+            disc_map = np.zeros(grid_flat)
+            drone_loc = grid.get_drones_vector()
             disc_map = disc_map + drone_loc
             state = np.append(drone_loc, disc_map)
+            target_found = False
             
-            # 2. Choose an action based on observation
-            action_idx = PG.choose_action(state)
+            # Run simulator
+            for itr in range(max_iterations):
+                # Choose an action based on observation
+                action_idx = PG.choose_action(state)
                 
-            # 3. Take action in the environment
-            reward = move_and_get_reward(drone, action_idx, disc_map)
-            rewards += reward
-            # 5. Check to see if target has been found
-            if reward >= 1:
-                break
-    
-        reward_list.append(rewards)
-        
+                # Take action in the environment
+                reward = move_and_get_reward(drone, action_idx, disc_map,itr)
+                
+                if reward >=0:
+                    target_found = True
+                    targets_found_test+=1
+                    
+                rewards += reward
+                
+                drone_loc = grid.get_drones_vector()
+                disc_map = disc_map + drone_loc
+                state = np.append(drone_loc, disc_map)
+                
+                # End Episode if target has been found
+                if target_found:
+                    break
+             
+            reward_list.append(rewards)
+            
+        action_values = np.array(action_values).reshape(-1,4)
+
+        plt.plot(action_values[:,0],label="Up")
+        plt.plot(action_values[:,1],label="Right")
+        plt.plot(action_values[:,2],label="Down")
+        plt.plot(action_values[:,3],label="Left")
+        plt.legend()
+        plt.title('Drone Search Values')
+        plt.xlabel('Episodes')
+        plt.ylabel('Values')
+        plt.rcParams["figure.figsize"]=(10,5)
+        plt.show()
+            
+    # EXPLORATION
     print("\n-------TRAINING------\n")
-    print("Target Found in: ",int(100*target_found/EPISODES),'%  of Episodes')
+    print("Target Found in: ",int(10000*targets_found/EPISODES)/100,'%  of Training Episodes')
     print("Total training discovery map:\n", global_disc_map.reshape(gridsize[0], gridsize[1]))
     
     # EXPLOTATION
     print("\n---------EXPLOITATION--------\n")
-    # Setup simulator
-
-    
+    print("Target Found in: ",int(10000*targets_found_test/EPISODES)/100,'%  of Test Episodes')
     print("Exploitation discovery map:\n", disc_map.reshape(gridsize[0], gridsize[1]))
-    print('\n',grid)
-    
-    plt.plot(reward_list)
-    plt.title('Drone Search')
-    plt.xlabel('Episodes')
-    plt.ylabel('Cost')
-    plt.rcParams["figure.figsize"]=(10,5)
+#        print('\n',grid)
+
+    reward_lists = np.mean(np.array(reward_list).reshape(-1,num_exec),axis=1)
+    plt.plot(reward_lists, label= ('Reward'))
+    plt.legend()
     plt.show()
