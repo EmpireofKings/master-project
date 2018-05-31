14c53b8c2c1dd2ba6535ff2e8b9fc272160c7537
ruchtem <34424489+ruchtem@users.noreply.github.com>
2018-05-31 13:01:40 +0200

    Added actor-critic with tensorboard

--- code/main/actor_critic/ActorCritic.py
+++ code/main/actor_critic/ActorCritic.py
@@ -6,7 +6,9 @@ Uses a 3 layer neural network as the policy network
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+
+from datetime import datetime
+
 
 
 class ActorCritic:
@@ -24,20 +26,57 @@ class ActorCritic:
         self.episode_actions = []
         self.episode_rewards = []
 
-        self.build_network()
-
         self.cost_history = []
 
         self.sess = tf.Session()
 
+        modifier = datetime.now().strftime("%m%d-%H%M%S") + " lnr=,disc=%s" % (self.discount_factor)
+        log_path = "_logs_/" + modifier
+
         # $ tensorboard --logdir=_logs_
-        # http://0.0.0.0:6006/
-        tf.summary.FileWriter("_logs_/", self.sess.graph)
+        self.writer = tf.summary.FileWriter(log_path)
+
+        #self.build_network()
+        self.trainer = tf.train.AdamOptimizer(self.lr)
+
+        self.X = tf.placeholder(tf.float32, shape=[None, self.n_x], name="X")
+        self.R = tf.placeholder(tf.float32, shape=[None, 1], name="discounted_rewards")
+        self.A = tf.placeholder(tf.float32, shape=[None, 4], name="actions")
+
+        with tf.variable_scope("policy_network", reuse=tf.AUTO_REUSE):
+            self.policy_network = tf.nn.softmax(self.policy_nn())
+
+            tf.summary.scalar("direction_up", self.policy_network[1][0])
+            tf.summary.scalar("direction_right", self.policy_network[1][1])
+            tf.summary.scalar("direction_down", self.policy_network[1][2])
+            tf.summary.scalar("direction_left", self.policy_network[1][3])
+
+        with tf.variable_scope("critic_network", reuse=tf.AUTO_REUSE):
+            self.critic_network = self.critic_nn()
+
+        with tf.name_scope("entropy"):
+            entropy = tf.reduce_sum(self.policy_network * tf.log(self.policy_network))
+
+        # Touch only the action we took
+        with tf.name_scope("policy_loss"):
+            policy = tf.reduce_sum(tf.multiply(self.policy_network, self.A))
+            self.policy_loss = policy*(self.R - self.critic_network) + 0.2 * entropy
+
+            tf.summary.scalar("policy_loss", tf.reduce_sum(self.policy_loss))
+
+        with tf.name_scope("critic_loss"):
+            self.critic_loss = (self.R - self.critic_network)**2
+
+            tf.summary.scalar("critic_loss", tf.reduce_sum(self.critic_loss))
+
+        self.train_ops = (self.trainer.minimize(self.policy_loss),
+                          self.trainer.minimize(self.critic_loss))
 
         self.sess.run(tf.global_variables_initializer())
+        self.writer.add_graph(self.sess.graph)
 
-        # 'Saver' op to save and restore all the variables
-        self.saver = tf.train.Saver()
+        self.iterations = 0
+
 
     def store_transition(self, s, a, r):
         """
@@ -66,88 +105,119 @@ class ActorCritic:
 
             Returns: index of action we want to choose
         """
-        
-        # Reshape necessary for as tesseract expects the rows as columns
-        observation = observation.reshape(1, -1)
-        
-        # Run forward propagation to get softmax probabilities
-        prob_weights = self.sess.run(self.action_NN, feed_dict={self.X: observation})
-        
+
+        prob_weights = self.get_policy(observation)
+
         return np.argmax(prob_weights)
 
+
+
     def learn(self, last_state, was_target_found):
-        # Discount episode reward
-        advantages, true_values = self.calc_advantages_and_true_values(last_state, was_target_found)
 
-        ## DEBUG OUTPUT commented out
-        #print("episode rewards", self.episode_rewards)
-        #print("losses", self.sess.run(self.loss, feed_dict={
-        #                        self.X: self.episode_states,
-        #                        self.actions_taken: self.episode_actions,
-        #                        self.advantages: advantages}))
-        #print("discount rewards\n", advantages)
-        #print("action values\n", self.episode_actions)
-        #print("episode_states\n", self.episode_states)
-        
-        #print("policy loss", self.sess.run(self.policy_loss, feed_dict={
-        #                            self.actions_taken: self.episode_actions,
-        #                            self.advantages: advantages}))
-        
-        #test_state = np.zeros((1, self.n_x))
-        #test_state[0,0] = 1
-        
-        #print("test_state", test_state)
-        #print("Before learning:", self.sess.run(self.network, feed_dict={self.X: test_state}))
-        ## DEBUG END
-        
-        # Train on available data    
-        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={
-                            self.X: self.episode_states,
-                            self.actions_taken: self.episode_actions,
-                            self.advantages: advantages,
-                            self.true_values: true_values})
+        # Final reference reward
+        if was_target_found:
+            discounted_reward = 0
+        else:
+            discounted_reward = self.get_critic(last_state)
 
-        #print("After learning:", self.sess.run(self.network, feed_dict={self.X: test_state}))
-        
+        #print(discounted_reward)
+
+        discounted_rewards = []
+        for i in reversed(range(len(self.episode_rewards))):
+            discounted_reward = self.episode_rewards[i] + self.discount_factor * discounted_reward
+            discounted_rewards.append(discounted_reward)
+        discounted_rewards = np.array(list(reversed(discounted_rewards))).ravel().reshape(-1, 1)
+
+        #print(self.episode_rewards)
+        #print(discounted_rewards.ravel())
+
+        feed_dict = {self.X: self.episode_states,
+                     self.A: self.episode_actions,
+                     self.R: discounted_rewards}
+
+
+        #var_policy = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="policy_network")
+        #var_critic = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="critic_network")
+
+        #action_gradients = tf.gradients(self.policy_loss, var_policy)
+        #value_gradients = tf.gradients(self.critic_loss, var_critic)
+
+        # Does not work with Adam optimizer!
+        #self.sess.run(self.trainer.apply_gradients(zip(action_gradients, var_policy)), feed_dict=feed_dict)
+        #self.sess.run(self.trainer.apply_gradients(zip(value_gradients, var_critic)), feed_dict=feed_dict)
+
+
+
+        # Will collect all summaries for tensorboard
+        summ = tf.summary.merge_all()
+
+        _, _, _, s = self.sess.run((self.train_ops,
+                                    self.policy_loss,
+                                    self.critic_loss,
+                                    summ), feed_dict=feed_dict)
+
+        self.writer.add_summary(s, self.iterations)
+
+        self.iterations += 1
+
+
+    def dense(self, input, kernel_shape):
+        # Have to use get_variable to have the scope adjusted correctly
+        kernel = tf.get_variable("kernel", kernel_shape, initializer=tf.random_normal_initializer(0.1, 0.2))
+        biases = tf.get_variable("biases", kernel_shape[1], initializer=tf.constant_initializer(0.1))
+        act = tf.nn.relu(tf.matmul(input, kernel) + biases)
+
+        if not tf.is_variable_initialized(kernel).eval(session=self.sess):
+            self.sess.run(tf.variables_initializer([kernel]))
+        if not tf.is_variable_initialized(biases).eval(session=self.sess):
+            self.sess.run(tf.variables_initializer([biases]))
+
+        tf.summary.histogram("weights", kernel)
+        tf.summary.histogram("biases", biases)
+        tf.summary.histogram("activations", act)
+
+        return act
+
+    def policy_nn(self):
+        with tf.variable_scope("hidden1"):
+            in_hidden1 = self.n_x
+            out_hidden1 = 30
+            hidden1 = self.dense(self.X, [in_hidden1, out_hidden1])
+        with tf.variable_scope("action_raw"):
+            out_policy = 4
+            policy_raw = self.dense(hidden1, [out_hidden1, out_policy])
+        return policy_raw
+
+    def get_policy(self, input):
+        return self.sess.run(self.policy_network, feed_dict={self.X: input.reshape(1, -1)})
+
+    def critic_nn(self):
+        with tf.variable_scope("hidden1"):
+            in_hidden1 = self.n_x
+            out_hidden1 = 30
+            hidden1 = self.dense(self.X, [in_hidden1, out_hidden1])
+        with tf.variable_scope("critic_raw"):
+            out_critic = 1
+            critic_raw = self.dense(hidden1, [out_hidden1, out_critic])
+        return critic_raw
+
+    def get_critic(self, input):
+        return self.sess.run(self.critic_network, feed_dict={self.X: input.reshape(1, -1)})
+
+
+
+
+
     def reset(self):
         # Reset the episode data, clear memory
         self.episode_states = np.empty((0,self.n_x), np.float32)
         self.episode_actions = []
         self.episode_rewards  = []
 
-    def calc_advantages_and_true_values(self, last_state, was_target_found):
-        count_steps = len(self.episode_rewards)
-        
-        # get critic values
-        values = self.sess.run(self.critic_NN, feed_dict={self.X: self.episode_states})
-        
-        # get value of last state
-        if was_target_found:
-            value_last_state = 10
-        else:
-            value_last_state = self.sess.run(self.critic_NN, feed_dict={self.X: last_state.reshape(1, -1)})
-        
-        # Calculate advantages
-        advantages = np.zeros(count_steps, dtype=np.float32)
-        true_values = np.zeros(count_steps, dtype=np.float32).reshape(-1, 1)
-        
-        true_values[-1] = value_last_state + self.episode_rewards[-1]
-        
-        for i in reversed(range(count_steps-1)):
-            true_values[i] = true_values[i+1] + self.episode_rewards[i]
-        
-        advantages = true_values - values
-        
-        #print("values", values)
-        #print("rewards", self.episode_rewards)
-        #print("true_values", true_values)
-        #print("advantages", advantages)
-        
-        
-        return advantages, true_values
 
-        
     def build_network(self):
+
+
         # Create placeholders
         with tf.name_scope('inputs'):
             self.X = tf.placeholder(tf.float32, shape=[None, self.n_x], name="X")
@@ -160,25 +230,28 @@ class ActorCritic:
             self.true_values = tf.placeholder(tf.float32,
                                               shape=[None, 1],
                                               name="true_values")
-            
+
         # First hidden layer
-        layer_1 = tf.layers.Dense(units=30,  # num output nodes
+        self.layer_1 = tf.layers.Dense(units=30,  # num output nodes
                                   activation=tf.nn.relu,
+                                  trainable=True,
                                   name="layer_1")
 
         # Raw action output layer, therefore no activation function
         # Also called logits sometimes, don't know why
-        layer_actions = tf.layers.Dense(units=self.n_y,  # num output nodes
-                                  name="layer_actions")
+        self.layer_actions = tf.layers.Dense(units=self.n_y,  # num output nodes
+                                             trainable=True,
+                                             name="layer_actions")
 
-        self.action_NN = tf.nn.softmax(layer_actions(layer_1(self.X)), name="layer_softmax_out")
-        
+        self.action_NN = tf.nn.softmax(self.layer_actions(self.layer_1(self.X)), name="layer_softmax_out")
+
         # Raw critic output layer, therefore no activation function
-        layer_critic = tf.layers.Dense(units=1,
-                                       name="layer_critic")
-        
-        self.critic_NN = layer_critic(layer_1(self.X))
-        
+        self.layer_critic = tf.layers.Dense(units=1,
+                                            trainable=True,
+                                            name="layer_critic")
+
+        self.critic_NN = self.layer_critic(self.layer_1(self.X))
+
         ## Policy loss
         # Element-wise multiplication, to touch only values for which we actually took an action
         # Note that the discounded rewards are normalized between 0 and 1 in order to fit to the softmax
@@ -187,23 +260,21 @@ class ActorCritic:
         # If the softmmax outup is [0, 0.9, 0, 0] but we have a low (negative before normalization) reward
         #   this value (0.9) will be decreased
         action_truth = tf.multiply(self.actions_taken, self.advantages)
-        
+
         # sum((y_hat-y_true)**2)
         # Touch only values for which we actually took an action
         action_prediction = tf.multiply(self.actions_taken, self.action_NN)
-        
+
         self.policy_loss = tf.reduce_sum((action_prediction - action_truth)**2)
-        
+
         ## Value (Critic) loss
         value_prediction = self.critic_NN
         self.value_loss = tf.reduce_sum((value_prediction - self.true_values)**2)
-        
+
         self.loss = self.value_loss + self.policy_loss
-        
-        
-        with tf.name_scope('train'):
-            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
-     
+
+
+
     def print_value_estimation(self):
         values = self.sess.run(self.critic_NN, feed_dict={self.X: self.episode_states})
         print("values of the critic:\n", values)
--- code/main/actor_critic/run_simulator.py
+++ code/main/actor_critic/run_simulator.py
@@ -9,8 +9,9 @@ from Simulator import *
 
 np.random.seed(1)
 
-EPISODES = 400
+EPISODES = 50
 rewards = []
+learning_rate = 1e-4
 
 gridsize = [5, 10]
 grid_flat = gridsize[0] * gridsize[1]
@@ -19,7 +20,7 @@ num_channels = 2
 input_size = grid_flat * num_channels
 
 action_size = 4 #number of actions to choose from
-e_greedy = 0.2
+e_greedy = 0.4
 grid_seed = 1
 max_iterations = int(grid_flat/2) #it will take this many actions to go to half of the cells in the grid
 
@@ -36,131 +37,131 @@ def move_and_get_reward(drone, action_idx, disc_map):
             # the penalty will increase linearly with each visit
             location_point = drone.get_position()
             location = location_point.get_y() * gridsize[1] + location_point.get_x()
-            return -disc_map[location]-1
+            return -disc_map[location]
             #return 0.1
     except (PositioningError, IndexError):
         # We hit an obstacle or tried to exit the grid
         return -10 # arbitrary
-            
+
 
 
 if __name__ == "__main__":
     reward_list = []
-    target_found = 0    
+    target_found = 0
     PG = ActorCritic(input_size, action_size,
-                     learning_rate=0.01,
+                     learning_rate,
                      reward_decay=0.99)
-    
+
     global_disc_map = np.zeros(grid_flat)
-    
-    
-    
+
+
+
     for episode in range(EPISODES):
         print("Episode", episode, end="\r")
-                
+
         # Setup simulator
         grid = Grid(gridsize[0],gridsize[1], seed=grid_seed)
         grid.set_obstacles(0) #No obstacles to start
         drone = Drone(grid, position=Point(0,0), id=99)
         grid.set_target()
-        
+
         disc_map = np.zeros(grid_flat)
         was_target_found = False
-        
+
         # Generate traning data
         for itr in range(max_iterations):
             # 1. Get the current state
             drone_loc = grid.get_drones_vector()
-            
+
             # Use smaller state space for the beginning
             disc_map += drone_loc
             state = np.append(drone_loc, disc_map)
-            
+
             # 2. Choose an action based on observation
             if np.random.randint(1,100)/100 < e_greedy:
                 action_idx = np.random.randint(0, action_size)
             else:
                 action_idx = PG.choose_action(state)
-                
+
             # 3. Take action in the environment
             reward = move_and_get_reward(drone, action_idx, disc_map)
-            
+
             # 4. Store transition for training
             PG.store_transition(state, action_idx, reward)
-            
+
             # 5. Check to see if target has been found
             if reward >= 1:
                 disc_map += grid.get_drones_vector()
                 target_found+=1
                 was_target_found = True
                 break
-        
-        
+
+
 
         global_disc_map += disc_map
         #print("\n-------TRAINING------\n")
-        
+
         # Get last state for learning
         disc_map += grid.get_drones_vector()
         last_state = np.append(grid.get_drones_vector(), disc_map)
-        
+
         # 5. Train neural network
         PG.learn(last_state, was_target_found)
-        
+
 #        reward_list.append(np.sum(PG.episode_rewards))
-        
-        #Reset the action, state, and reward lists. 
+
+        #Reset the action, state, and reward lists.
         #Keeping this is like replay memory (full sampling)
         PG.reset()
-        
+
         grid = Grid(gridsize[0],gridsize[1], seed=grid_seed)
         grid.set_obstacles(0) #No obstacles to start
         drone = Drone(grid, position=Point(0,0), id=99)
         grid.set_target()
         rewards = 0
         disc_map = np.zeros(grid_flat)
-    
+
         # Run simulator
         for itr in range(max_iterations):
             # 1. Get the current state
             drone_loc = grid.get_drones_vector()
-            
+
             # Use smaller state space for the beginning
             disc_map = disc_map + drone_loc
             state = np.append(drone_loc, disc_map)
-            
+
             # 2. Choose an action based on observation
             action_idx = PG.choose_action(state)
-                
+
             # 3. Take action in the environment
             reward = move_and_get_reward(drone, action_idx, disc_map)
             rewards += reward
-            
+
             PG.store_transition(state, action_idx, reward)
-            
+
             # 5. Check to see if target has been found
             if reward >= 1:
                 break
-    
+
         reward_list.append(rewards)
-        
+
     print("\n-------TRAINING------\n")
     print("Target Found in: ",int(100*target_found/EPISODES),'%  of Episodes')
     print("Total training discovery map:\n", global_disc_map.reshape(gridsize[0], gridsize[1]))
-    
+
     # EXPLOTATION
     print("\n---------EXPLOITATION--------\n")
     # Setup simulator
 
-    
+
     print("Exploitation discovery map:\n", disc_map.reshape(gridsize[0], gridsize[1]))
     print('\n',grid)
-    
-    PG.print_value_estimation()
-    
-    plt.plot(reward_list)
-    plt.title('Drone Search')
-    plt.xlabel('Episodes')
-    plt.ylabel('Cost')
-    plt.rcParams["figure.figsize"]=(10,5)
-    plt.show()
+
+    #PG.print_value_estimation()
+
+    #plt.plot(reward_list)
+    #plt.title('Drone Search')
+    #plt.xlabel('Episodes')
+    #plt.ylabel('Cost')
+    #plt.rcParams["figure.figsize"]=(10,5)
+    #plt.show()
